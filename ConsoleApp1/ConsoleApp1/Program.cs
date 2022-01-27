ea3a2969598af3b15794fcae290c870976ecf0b8
varshithagudimetla <98520285+varshithagudimetla@users.noreply.github.com>
2022-01-27 21:03:30 +0530

    new txt added
    change in Bl logic

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,6 +8,8 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine(" i like to learn new things slowly ");
+            Console.WriteLine(" i like to learny ");
+            
         }
     }
 }
